35ccbed7aa14ad1ed59f5b4ad26aab98db08de76
ShivMoh <shiveshmohamed1234@gmail.com>
2023-12-15 14:40:38 -0400

    Did i do anything?

--- inc/controller/validators/Validator.php
+++ inc/controller/validators/Validator.php
@@ -14,21 +14,21 @@ class Validator {
         return count($sanitized) === 1 ? $sanitized[0] : $sanitized;
     }
 
-        public function isString($input){
-            return is_string($input);
-        }
-    
-        public function isInt($input){
-            return filter_var($input, FILTER_VALIDATE_INT) !== false;
-        }
-    
-        public function isFloat($input){
-            return filter_var($input, FILTER_VALIDATE_FLOAT) !== false;
-        }
-    
-        public function isEmail($input){
-            return filter_var($input, FILTER_VALIDATE_EMAIL) !== false;
-        }
+    public function isString($input){
+        return is_string($input);
+    }
+
+    public function isInt($input){
+        return filter_var($input, FILTER_VALIDATE_INT) !== false;
+    }
+
+    public function isFloat($input){
+        return filter_var($input, FILTER_VALIDATE_FLOAT) !== false;
+    }
+
+    public function isEmail($input){
+        return filter_var($input, FILTER_VALIDATE_EMAIL) !== false;
+    }
 
 
 }
