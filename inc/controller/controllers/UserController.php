5736871ca93b7e55ebc3bff1f29166d16891e32a
Rey <reynardetwaroo4@gmail.com>
2023-12-19 21:28:29 -0400

    Made the logout 10x better (trust)

--- inc/controller/controllers/UserController.php
+++ inc/controller/controllers/UserController.php
@@ -60,13 +60,25 @@ class UserController extends BaseController {
     }
 
     public function logout() {
+        // Start the session if it's not already started
         if (session_status() == PHP_SESSION_NONE) {
             session_start();
         }
-        unset($_SESSION['user_id']);
-        unset($_SESSION['user_role']);
-        $this->anchor("user");
 
+        // Unset all of the session variables.
+        $_SESSION = array();
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        // Finally, destroy the session.
+        session_destroy();
     }
 
     public function registerPage() {
